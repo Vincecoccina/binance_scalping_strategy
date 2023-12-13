ac75bddec40b8eedbd80b6658bce48b32825fdb6
Vince <v.coccina@orange.fr>
2023-12-14 00:45:40 +0100

    Logique scalping

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pandas_ta as ta
 import numpy as np
+import plotly.graph_objects as go
 import os
 from binance.client import Client
 from binance import ThreadedWebsocketManager
@@ -13,107 +14,176 @@ load_dotenv()
 api_key = os.getenv("API_KEY_TEST")
 secret_key = os.getenv("SECRET_KEY_TEST")
 
-client = Client(api_key=api_key, api_secret=secret_key, tld='com',testnet=False)
+# Initialisation de l'API Binance avec les clés API
+client = Client(api_key=api_key, api_secret=secret_key, tld='com', testnet=True)
 
-# I - Récupérer les données historiques
-# ------------------------------------------
-# Fonction de récupération des données
-def get_historical_data(symbol, interval, limit=500):
-    klines = client.get_klines(symbol=symbol, interval=interval, limit=limit)
-    df = pd.DataFrame(klines, columns=['Open_time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close_time', 'Quote_asset_volume', 'Number_of_trades', 'Taker_buy_base_asset_volume', 'Taker_buy_quote_asset_volume', 'Ignore'])
-    df[['Open', 'High', 'Low', 'Close']] = df[['Open', 'High', 'Low', 'Close']].astype(float)
-    return df
+# symbol = 'TRXUSDT'
+symbol = 'XRPUSDT'
+# symbol = 'ETHUSDT'
+# symbol = 'BTCUSDT'
+# symbol = 'XMRUSDT'
+# symbol = 'BNBUSDT'
+# symbol = 'LTCUSDT'
+# symbol = 'SOLUSDT'
 
-# Récupération des données + netoyage
-data = get_historical_data('BTCUSDT', "1d")
-data = data.drop(columns=['Quote_asset_volume', 'Close_time', 'Number_of_trades','Taker_buy_base_asset_volume', 'Taker_buy_quote_asset_volume', 'Ignore' ])
-data['Open_time'] = pd.to_datetime(data['Open_time'], unit='ms')
-data.rename(columns={'Open_time': 'Date'}, inplace=True)
-data.set_index('Date', inplace=True)
+class ScalpingTrader():
+    
+    def __init__(self, symbol, bar_length):
+        self.symbol = symbol
+        self.bar_length = bar_length
+        self.available_intervals = ["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]
+    
+    def start_trading(self, historical_days):
+        
+        self.twm = ThreadedWebsocketManager()
+        self.twm.start()
+        
+        if self.bar_length in self.available_intervals:
+            self.get_most_recent(symbol = self.symbol, interval = self.bar_length,
+                                 days = historical_days)
+            self.twm.start_kline_socket(callback = self.stream_candles,
+                                        symbol = self.symbol, interval = self.bar_length)
+            
 
-# II - Calculer SMA et SLOPE
-# ------------------------------------------
-# Calculer le SMA (Le SMA aide a identifier la tendance du marché, un SMA croissant indique une tendance haussière)
-# Le SMA calcule la moyenne des prix de cloture sur une période donnée => tendance générale
-def calculate_sma(data, length: int):
-    return ta.sma(data['Close'], length)
+    def get_most_recent(self, symbol, interval, days):
+            now = datetime.utcnow()
+            past = str(now - timedelta(days = days))
 
-# Calculer le SLOPE (utilisé pour identifier la direction et la force de la tendance)
-def calculate_slope(series, period: int = 5):
-    slopes = [0 for _ in range(period-1)]
-    for i in range(period-1, len(series)):
-        x = np.arange(period)
-        y = series[i-period+1:i+1].values
-        slope = np.polyfit(x, y, 1)[0]
-        percent_slope = (slope / y[0]) * 100
-        slopes.append(percent_slope)
-    return slopes
+            bars = client.get_historical_klines(symbol = symbol, interval = interval,
+                                                start_str=past, end_str=None, limit=1000)
+            df = pd.DataFrame(bars)
+            df["Date"] = pd.to_datetime(df.iloc[:,0], unit = "ms")
+            df.columns = ["Open Time", "Open", "High", "Low", "Close", "Volume",
+                        "Clos Time", "Quote Asset Volume", "Number of Trades",
+                        "Taker Buy Base Asset Volume", "Taker Buy Quote Asset Volume", "Ignore", "Date"]
+            df = df[["Date", "Open", "High", "Low", "Close", "Volume"]].copy()
+            df.set_index("Date", inplace = True)
+            df[['High', 'Low', 'Close', 'Volume']] = df[['High', 'Low', 'Close', 'Volume']].astype(float)
+            for column in df.columns:
+                df[column] = pd.to_numeric(df[column], errors = "coerce")
+            df["Complete"] = [True for row in range(len(df)-1)] + [False]
+            self.data = df
+            self.calculate_indicators()
+    
+    def calculate_vwap_signal(self, backcandles=15):
+        VWAPsignal = [0] * len(self.data)
 
-# Ajout des calcules SMA et SLOPE au DataFrame
-data["SMA_10"] = calculate_sma(data, 10)
-data["SMA_20"] = calculate_sma(data, 20)
-data["SMA_30"] = calculate_sma(data, 30)
-data["Slope"] = calculate_slope(data["SMA_20"])
-data.dropna(inplace=True)
-data.reset_index(inplace=True, drop=True)
+        for row in range(backcandles, len(self.data)):
+            upt = 1
+            dnt = 1
+            for i in range(row - backcandles, row + 1):
+                if max(self.data["Open"].iloc[i], self.data["Close"].iloc[i]) >= self.data["VWAP"].iloc[i]:
+                    dnt = 0
+                if min(self.data["Open"].iloc[i], self.data["Close"].iloc[i]) <= self.data["VWAP"].iloc[i]:
+                    upt = 0
+            if upt == 1 and dnt == 1:
+                VWAPsignal[row] = 3
+            elif upt == 1:
+                VWAPsignal[row] = 2
+            elif dnt == 1:
+                VWAPsignal[row] = 1
 
-# III - Déterminer la tendance (haussière, baissière ou absence de tendance)
-# -------------------------------------------------------------------------
-# Fonction pour connaitre la tendance
-def determinate_trend(data):
-    if data["SMA_10"] > data["SMA_20"] > data["SMA_30"]:
-        return 2 # Tendance haussière
-    elif data["SMA_10"] < data["SMA_20"] < data["SMA_30"]:
-        return 1 # Tendance baissière
-    else:
-        return 0 # Pas de tendance
+        self.data['VWAPSignal'] = VWAPsignal
+    
+    def total_signal(self, l):
+        if (self.data["VWAPSignal"].iloc[l] == 2 and
+            self.data["Close"].iloc[l] <= self.data['Lower Band'].iloc[l] and
+            self.data["RSI"].iloc[l] < 45):
+            return 2
+        if (self.data["VWAPSignal"].iloc[l] == 1 and
+            self.data["Close"].iloc[l] >= self.data['Upper Band'].iloc[l] and
+            self.data["RSI"].iloc[l] > 55):
+            return 1
+        return 0
 
-# Ajout du calcule de la TENDANCE au DataFrame
-data["Trend"] = data.apply(determinate_trend, axis=1)
-
-# IV - Savoir si les chandeliers japonais au dessus ou en dessous de la moyenne mobile (MA)
-# ------------------------------------------------------------------------------------------
-# Fonction qui compare la bougie à la moyenne mobile
-def check_candles(data, backcandles, ma_column):
-    categories = [0 for _ in range(backcandles)]
-    for i in range(backcandles, len(data)):
-        if all(data["Close"][i-backcandles:i] > data[ma_column][i-backcandles:i]):
-            categories.append(2) # Tendance Haussière
-        elif all(data["Close"][i-backcandles:i] < data[ma_column][i-backcandles:i]):
-            categories.append(1) #Tendance Baissière
+    def calculate_total_signal(self):
+        TotSignal = [0]*len(self.data)
+        backcandles = 15  # ou toute autre valeur appropriée
+        for row in range(backcandles, len(self.data)):
+            TotSignal[row] = self.total_signal(row)
+        self.data['TotalSignal'] = TotSignal
+    
+    def pointposbreak(self, x):
+        if x['TotalSignal'] == 1:
+            return x['High'] + 1e-4
+        elif x['TotalSignal'] == 2:
+            return x['Low'] - 1e-4
         else:
-            categories.append(0)
+            return np.nan
 
-    return categories
+    def calculate_pointposbreak(self):
+        self.data['pointposbreak'] = self.data.apply(self.pointposbreak, axis=1)
 
-# Ajout du calcule de la CATEGORY au DataFrame
-data["Category"] = check_candles(data, 5, "SMA_20")
+    def calculate_indicators(self):
+        self.data['VWAP'] = ta.vwap(self.data['High'], self.data['Low'], self.data['Close'], self.data['Volume'])
+        self.data['RSI'] = ta.rsi(self.data['Close'], length=16)
+        self.data.ta.bbands(length=14, std=2.0, append=True)
+        self.data["Upper Band"] = self.data["BBU_14_2.0"]
+        self.data["Lower Band"] = self.data["BBL_14_2.0"]
+        self.calculate_vwap_signal()
+        self.calculate_total_signal()
+        self.calculate_pointposbreak()
+     
+                    
+    def stream_candles(self, msg):
+        event_time = pd.to_datetime(msg["E"], unit = "ms")
+        start_time = pd.to_datetime(msg["k"]["t"], unit = "ms")
+        first   = float(msg["k"]["o"])
+        high    = float(msg["k"]["h"])
+        low     = float(msg["k"]["l"])
+        close   = float(msg["k"]["c"])
+        volume  = float(msg["k"]["v"])
+        complete=       msg["k"]["x"]
+        print("Time: {} | Price: {}".format(event_time, close))
+        new_data = pd.DataFrame({
+        "Open": [first],
+        "High": [high],
+        "Low": [low],
+        "Close": [close],
+        "Volume": [volume],
+        "Complete": [complete]
+        }, index=[start_time])
 
-# V - Confirmation de la tendance avec l'indicateur ADX (Déterminer la force d'une tendance)
-# ------------------------------------------------------------------------------------------
-# Calcule de l'ADX
-data.ta.adx(append=True)
-# Fonction qui génère un signal pour la tendnace basé sur l'ADX
-def generate_trend_signal(data, threshold=40):
-    trend_signal = []
-    for i in range(len(data)):
-        if data["ADX"][i] > threshold:
-            if data["DMP"][i] > data["DMN"][i]:
-                trend_signal.append(2) # Tendance haussière
-            else:
-                trend_signal.append(1) # Tendance Baissière
-        else:
-            trend_signal.append(0) # Pas de tendance claire
-    return trend_signal
+        # Concaténer avec les données existantes
+        self.data = pd.concat([self.data, new_data])
+        # Recalcule des indicateurs
+        self.calculate_indicators()
+    
+    def plot_data(self):
+        # Préparer les données de signal
+        achat_signals = self.data[self.data['TotalSignal'] == 2]
+        vente_signals = self.data[self.data['TotalSignal'] == 1]
 
-data = data.rename(columns=lambda x: x[:3] if x.startswith("ADX") else x)
-data = data.rename(columns=lambda x: x[:3] if x.startswith("DM") else x)
+        # Créer le graphique à chandeliers
+        fig = go.Figure(data=[go.Candlestick(x=self.data.index,
+                                             open=self.data["Open"],
+                                             high=self.data["High"],
+                                             low=self.data["Low"],
+                                             close=self.data["Close"])])
+        # Ajouter des lignes pour les indicateurs
+        fig.add_trace(go.Scatter(x=self.data.index, y=self.data["VWAP"], mode="lines", name="VWAP", line=dict(color="blue")))
+        fig.add_trace(go.Scatter(x=self.data.index, y=self.data["Lower Band"], mode="lines", name="Lower Band", line=dict(color="crimson")))
+        fig.add_trace(go.Scatter(x=self.data.index, y=self.data["Upper Band"], mode="lines", name="Upper Band", line=dict(color="green")))
 
-# Ajout du calcule du SIGNAL DE LA TENDANCE au DataFrame
-data["Trend Signal"] = generate_trend_signal(data)
-# Ajout de la CONFIRMATION DU SIGNAL DE LA TENDANCE au DataFrame
-data["Confirmed Signal"] = data.apply(lambda row: row["Category"] if row["Category"] == row["Trend Signal"] else 0, axis=1)
-data = data[data["Confirmed Signal"]!=0]
+        # Ajouter des marqueurs pour les signaux
+        fig.add_trace(go.Scatter(x=achat_signals.index, y=achat_signals['Close'],
+                                 mode='markers', marker=dict(color='green', size=10),
+                                 name='Signal Achat'))
+        fig.add_trace(go.Scatter(x=vente_signals.index, y=vente_signals['Close'],
+                                 mode='markers', marker=dict(color='red', size=10),
+                                 name='Signal Vente'))
+        
+        # Afficher le graphique
+        fig.show()
 
+trader = ScalpingTrader(symbol=symbol, bar_length="5m")
+trader.start_trading(historical_days = 2)
 
-print(data)
+run_time = 30 
+
+time.sleep(run_time)
+
+trader.twm.stop()
+filtered_data = trader.data[trader.data["TotalSignal"]!=0]
+print(trader.data)
+trader.plot_data()
